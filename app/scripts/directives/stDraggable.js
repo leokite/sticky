bb05e42718c6c0148fff5c898f8a379b26f0d753
leokite <yuji.kazama.dev@gmail.com>
2014-02-10 23:01:57 +0900

    set containment

--- app/scripts/directives/stDraggable.js
+++ app/scripts/directives/stDraggable.js
@@ -4,13 +4,10 @@ angular.module('stickyApp')
     return {
     // jshint unused: vars
       link: function($scope, element, attrs) {
-        // $(element).animate({
-          // left: $scope.sticky.left,
-          // top: $scope.sticky.top
-        // }, 500);
 
         $(element).draggable({
-          stack: ".sticky"
+          stack: ".sticky",
+          containment: [0, 55, 2000, 2000]
         });
 
         socket.on('moveSticky', function(stickies) {
