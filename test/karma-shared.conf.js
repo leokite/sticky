c70407931d243ca6d72439c94e1ea39ab43c9c4a
leokite <yuji.kazama.dev@gmail.com>
2014-03-01 23:11:33 +0900

    firefox

--- test/karma-shared.conf.js
+++ test/karma-shared.conf.js
@@ -45,7 +45,7 @@ module.exports = function() {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: ['Firefox'],
 
     // preprocessors: {
       // 'app/scripts/**/*.js': ['coverage']
